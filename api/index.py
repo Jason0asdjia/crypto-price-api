e3dfa3c7023b0a2ba1561d1663ea50ee41821f67
rice <v7yo@outlook.com>
2025-12-14 07:39:32 +0900

    禁用本地测试用代码

--- api/index.py
+++ api/index.py
@@ -269,5 +269,5 @@ def update_account_snapshot():
         }), 500
     
 # 仅本地测试用
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000, debug=True)
